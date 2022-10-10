368ad8f40b2a67f5c59367c4ef10ac563cab17a3
JakobTPT <114664753+JakobTPT@users.noreply.github.com>
2022-10-10 10:40:15 +0000

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -1,16 +1,11 @@
-function checkTemp() {
-    Temp = 58
+function checkTemp(Temp) {
     if (Temp < 20 && Temp > 0) {
-        console.log(-1)
+        return -1
     }
-    else if (Temp < 40 && Temp > 21) {
-        console.log(0)
+    else if (Temp < 40 && Temp > 20) {
+        return 0
     }
-    else if (Temp < 60 && Temp > 41) {
-        console.log(1)
-    } else {
-        console.log("ükski tingimus ei vastanud sisendile")
+    else if (Temp < 60 && Temp > 40) {
+        return 1
     }
 }
-
-checkTemp();
